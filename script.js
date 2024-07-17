a898f0e39d2c4d5cfeb31565bff3318883a8c354
mali7422 <muhammedalical123@gmail.com>
2024-07-17 08:56:48 +0300

    Karakterin kosusu duzeltildi, reload sistemi eklendi, carpma algılama iyilestirildi

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 const areaWidth = document.querySelector('#container').offsetWidth; // Oyun alanının genişliği
 const areaHeight = document.querySelector('#container').offsetHeight; // Oyun alanının yüksekliği
 const gamearea = document.getElementById('container'); // Oyun alanı elementi
-const character = document.querySelector('#character');
+const number = document.getElementById('number');
+const knowledge = document.getElementById('knowledge');
 
 const jumpLongHeight = 150; // Uzun zıplama yüksekliği
 const jumpShortHeight = 120; // Kısa zıplama yüksekliği
-const runSpeed = 5; // Koşu hızı
-const jumpSpeed = 5; // Zıplama hızı
-
+const jumpSpeed = 4; // Zıplama hızı
 const characterImgPath = 'Character/character';
+const startSpeed = 4;
+
+let gameOver = false;
+let runSpeed = startSpeed;
+let time = 0;
 
 let keydownTime; // Boşluk tuşuna basılma zamanı
 
-let m = false;
-
 let player = {
-    object: document.querySelector('#player'), // Oyuncu DOM elementi
+    object: document.querySelector('#character'), // Oyuncu DOM elementi
     x: 0, // Oyuncunun x pozisyonu
     y: 0, // Oyuncunun y pozisyonu
 
     height: 50, // Oyuncunun boyu
-    width: 50, // Oyuncunun genişliği
+    width: 30, // Oyuncunun genişliği
 
     shortJump: false, // Kısa zıplama durumu
     longJump: false, // Uzun zıplama durumu
@@ -33,15 +35,38 @@ let player = {
 
 let obstacles = []; // Engellerin listesi
 
-var game = setInterval(function () {
-    play();
-}, 15); // Oyun döngüsü, 20ms aralıklarla `play()` fonksiyonunu çağırır
+let gameStarted = false;
 
-var view = setInterval(function () {
-    display(); // Ekranı güncelle
-}, 15);
+var game;
+var view;
+
+var tab = document.createElement('div');
+gamearea.appendChild(tab);
+tab.id = "tab";
+tab.textContent = "Start";
+
+
+function start() {
+    if(gameStarted == false) {
+        
+        setTimeout(function(){tab.remove();}, 40);
+
+        game = setInterval(function () {
+            play();
+        }, 10); // Oyun döngüsü, 20ms aralıklarla `play()` fonksiyonunu çağırır
+    
+        view = setInterval(function () {
+            display(); // Ekranı güncelle
+        }, 10);
+
+        gameStarted = true;
+    }
+};
 
 function play() {
+
+    time+=1;
+    
     player.x += runSpeed; // Oyuncunun x pozisyonunu koşu hızıyla güncelle
 
     if (player.jumpStarted) {
@@ -95,6 +120,9 @@ function display() {
     }
 
     changeImage();
+    
+    increaseScore();
+
 }
 
 function convertToDisplayLocations(o) {
@@ -121,27 +149,27 @@ function adjustCameraView(o) {
     return result;
 };
 
-
+setTimeout(obstacle, getRandomInterval(500,3000));
 
 function newObstacle() {
     // Yeni bir engel oluşturur ve DOM'a ekler
     var o = {
         x: player.x + areaWidth - 60, // Engelin x pozisyonu
         y: 0, // Engelin y pozisyonu
-        height: Math.ceil((Math.random()*2))*40, // Engelin yüksekliği
+        height: Math.ceil((Math.random()*2))*20+20, // Engelin yüksekliği
         width: 40, // Engelin genişliği
         object: null,
     };
 
     o.object = document.createElement('div'); // Yeni bir DOM elementi oluştur
-
+    
     o.object.style.left = "0px";
     o.object.style.top = "0px";
     o.object.style.height = "0px";
     o.object.style.width = "0px";
     // Ne için olduğunu sor!!!
 
-    o.object.className = 'obstacle'; // CSS sınıfı ekle
+    o.object.className = 'obstacle';
 
     gamearea.appendChild(o.object); // Oyun alanına ekler
     o.object.className = 'obstacle';
@@ -153,23 +181,30 @@ function newObstacle() {
 
 document.addEventListener('keydown', keydown);
 document.addEventListener('keyup', keyup);
+gamearea.addEventListener('mousedown', keydown);
+gamearea.addEventListener('mouseup', keyup);
+gamearea.addEventListener('touchstart', keydown);
+gamearea.addEventListener('touchend', keyup);
 
 function keydown(e) {
-    if (e.key === ' ') {
+    if (e.key === ' '||e.button === 0||e.type === 'touchstart') {
         keydownTimeControl(); // Boşluk tuşuna basılma olayını kontrol et
-        m = true;
+        start();
+
+        if(gameOver) {
+            restart();
+            gameOver = false;
+        }
     }
 };
 
 function keyup(e) {
-    if (e.key === ' ') {
+    if (e.key === ' '||e.button === 0||e.type === 'touchend') {
         keyupTimeControl(); // Boşluk tuşuna bırakılma olayını kontrol et
-        m = false;
     }
 };
 
 function keydownTimeControl() {
-    console.log("y :" + player.y);
     if(!keydownTime) {
         keydownTime = new Date().getTime(); // Boşluk tuşuna basılma zamanını al
     }
@@ -185,7 +220,6 @@ function keyupTimeControl() {
         const duration = keyupTime - keydownTime; // Tuş basılı kalma süresini hesapla
 
         if (duration <= 300) {
-            console.log(duration);
             player.shortJump = true; // Kısa zıplama modunu kapat
             player.longJump = false; // Uzun zıplama modunu aç
         }
@@ -198,7 +232,7 @@ function getRandomInterval(min,max) {
     return Math.random()*(max-min)+min;
 };
 
-setTimeout(obstacle, getRandomInterval(500,3000));
+
 
 function obstacle() {
     obstacles.push(newObstacle());
@@ -208,22 +242,41 @@ function distanceControl() {
     if(obstacles.length>=1) {
         let player_right = player.x + player.width;
         let player_left = player.x;
-        let obstacle_right = obstacles[0].x + obstacles[0].width - 3;
-        let obstacle_left = obstacles[0].x + 3;
-        let obstacle_height = obstacles[0].height - 3;
+        let obstacle_right = obstacles[0].x + obstacles[0].width;
+        let obstacle_left = obstacles[0].x;
+        let obstacle_height = obstacles[0].height - 10;
         
         if((player_right>=obstacle_left)&&(player_left<=obstacle_right)&&(player.y>=0)&&(player.y<=obstacle_height)) {
             clearInterval(game);
             clearInterval(view);
+
+            tab = document.createElement('div');
+            gamearea.appendChild(tab);
+            tab.id = "tab";
+            tab.textContent = "Game Over";
+
+            gameOver = true;
+
         }
     }
 };
 
 function changeImage() {
 
-    let mod = (player.x/runSpeed)%32;
-    if(mod%4==0){
-        character.src = characterImgPath+(mod/4+1)+".png";
+    let mod = (time)%64;
+    if(mod%8==0){
+        character.src = characterImgPath+(mod/8+1)+".png";
     }
-    
+};
+
+function increaseScore() {
+
+    player.score = Math.floor(player.x/100);
+
+    number.innerText = " " +  player.score;
+
+};
+
+function restart() {
+    location.reload();
 };
